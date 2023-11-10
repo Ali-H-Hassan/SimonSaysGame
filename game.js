350326090c574ccf2cd852348e19b5fa12660d94
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-11-11 00:02:55 +0200

    Initialiszing variables

---
+++ game.js
@@ -0,0 +1,6 @@
+//Initialization
+const level_title = document.getElementById('level-title');
+const buttons = document.querySelectorAll('.btn');
+const simonPattern = [];
+const userPattern = [];
+const buttonColors = ['red', 'blue', 'green', 'yellow'];
