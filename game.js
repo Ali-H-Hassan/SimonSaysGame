1200876010d2f122dbcc2d170d3fd40bd42a9e28
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-11-11 21:31:45 +0200

    User said

--- game.js
+++ game.js
@@ -1,43 +1,70 @@
 //Initialization
-const level_title = document.getElementById('level-title');
-const buttons = document.querySelectorAll('.btn');
+const level_title = document.getElementById("level-title");
+const buttons = document.querySelectorAll(".btn");
 const simonPattern = [];
 const userPattern = [];
-const buttonColors = ['red', 'blue', 'green', 'yellow'];
+const buttonColors = ["red", "blue", "green", "yellow"];
 let start = false;
 let level = 0;
 
 function nextSequence() {
-    userPattern.length = 0;
-    level++;
-    level_title.textContent = 'Level ' + level;
-    const randomNb = Math.floor(Math.random() * 4);
-    const randomColor = buttonColors[randomNb];
-    simonPattern.push(randomColor);
+  userPattern.length = 0;
+  level++;
+  level_title.textContent = "Level " + level;
+  const randomNb = Math.floor(Math.random() * 4);
+  const randomColor = buttonColors[randomNb];
+  simonPattern.push(randomColor);
 
-    sequenceAnimation(simonPattern)
+  sequenceAnimation(simonPattern);
 }
 
 function sequenceAnimation(sequence) {
-    let i = 0;
-    const interval = setInterval(function () {
-        animateButton(sequence[i]);
-        playSound(sequence[i]);
-        i++;
-        if (i >= sequence.length) {
-            clearInterval(interval);
-        }
-    }, 1000);
+  let i = 0;
+  const interval = setInterval(function () {
+    animateButton(sequence[i]);
+    playSound(sequence[i]);
+    i++;
+    if (i >= sequence.length) {
+      clearInterval(interval);
+    }
+  }, 1000);
 }
 
 function animateButton(color) {
-    const button = document.getElementById(color);
-    button.classList.add('pressed');
-    setTimeout(function () {
-        button.classList.remove('pressed')
-    }, 500);
+  const button = document.getElementById(color);
+  button.classList.add("pressed");
+  setTimeout(function () {
+    button.classList.remove("pressed");
+  }, 500);
 }
 
 function playSound(name) {
-    const audio = new Audio('sounds/' + name + '.mp3');
+  const audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function handleClick(e) {
+  const userColor = e.target.id;
+  userPattern.push(userColor);
+  animateButton(userColor);
+  playSound(userColor);
+  checkAnswer(userPattern.length - 1);
+}
+
+function checkAnswer(level) {
+  if (simonPattern[level] === userPattern[level]) {
+    if (userPattern.length === simonPattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    gameOver();
+  }
+}
+function gameOver() {
+  level_title.textContent = "Game Over, Press Any Key to Restart";
+  start = false;
+  simonPattern = false;
 }
